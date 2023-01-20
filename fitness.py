9ab971b9af009304adaa4f4ca902dbd9290ca810
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-20 11:51:54 +0200

    Lisätty dockstring

--- fitness.py
+++ fitness.py
@@ -4,7 +4,7 @@
 #Muuttujat
 pituus = 163 # Pituus senttimetreinä (cm)
 paino = 57.1 # Paino kilogrammoina (kg)
-pituus_teksti = input('Kuinka pitkä olet? (cm): ')
+pituus_teksti = input('Kuinka pitkä olet? (cm): ') # Input lukee käyttäjän vastaukset
 paino_teksti = input('Kuinka paljon painat? (kg): ')
 pituus = float(pituus_teksti) # Muutetaan liukuluvuksi
 paino = float(paino_teksti)
@@ -20,6 +20,17 @@ print('Painoindeksisi on ', bmi)
 '''
 # Määritellään funktio painoindeksi laskentaan
 def laske_bmi(paino, pituus):
+
+    """ Laskee painoindeksin (BMI)
+
+    Args:
+        paino (liukuluku/float): paino (kg)
+        pituus (liukuluku/float): pituus (cm)
+
+    Returns:
+        float: painoindeksi 2:n desimaalin tarkkuudella
+    """
+
     pituus = pituus / 100 # Muutetaan pituus metreiksi
     bmi = paino / pituus**2 # BMI:n matemaattinen laskukaava, rivit 23 ja 24
     bmi = round(bmi, 2) # Pyöristetään bmi kahteen desimaaliin, vaihtoehto 1
