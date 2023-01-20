c360b1e15c41f5dde270516f41f1521fb0dd18ab
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-20 14:21:10 +0200

    Rasvaprosentti + BMI

--- fitness.py
+++ fitness.py
@@ -1,23 +1,20 @@
 # Sovellus painoindeksin ja kehon rasvaprosentin laskemiseen
 #===========================================================
 
-#Muuttujat
-pituus = 163 # Pituus senttimetreinä (cm)
-paino = 57.1 # Paino kilogrammoina (kg)
+# Muuttujat
+# Kysytään käyttäjältä tiedot
+
 pituus_teksti = input('Kuinka pitkä olet? (cm): ') # Input lukee käyttäjän vastaukset
 paino_teksti = input('Kuinka paljon painat? (kg): ')
-pituus = float(pituus_teksti) # Muutetaan liukuluvuksi
+ika_teksti = input('Minkä ikäinen olet? ')
+sukupuoli_teksti = input('Sukupuoli mies, vastaa 1, N -nainen: vastaa 0: ')
+
+# Muutetaan vastaukset liukuluvuiksi
+pituus = float(pituus_teksti)
 paino = float(paino_teksti)
+ika = float(ika_teksti)
+sukupuoli = float(sukupuoli_teksti)
 
-'''
-pituus_metreina = pituus / 100
-
-# Lasketaan painoindeksi (BMI)
-bmi = paino / pituus_metreina **2
-
-print('Painoindeksisi on ', bmi)
-
-'''
 # Määritellään funktio painoindeksi laskentaan
 def laske_bmi(paino, pituus):
 
@@ -28,15 +25,34 @@ def laske_bmi(paino, pituus):
         pituus (liukuluku/float): pituus (cm)
 
     Returns:
-        float: painoindeksi 2:n desimaalin tarkkuudella
+        float: painoindeksi kahden desimaalin tarkkuudella
     """
 
     pituus = pituus / 100 # Muutetaan pituus metreiksi
     bmi = paino / pituus**2 # BMI:n matemaattinen laskukaava, rivit 23 ja 24
     bmi = round(bmi, 2) # Pyöristetään bmi kahteen desimaaliin, vaihtoehto 1
-    # print('Painoindeksisi on ', bmi)
     return bmi # Palauta bmi-laskukaavan määritelmä allaolevaan koodiin
 
 bmi = laske_bmi(paino, pituus)
-# x = round(bmi,2) # Pyöristetään bmi kahteen desimaaliin, vaihtoehto 2
-print('Painoindeksisi on:', bmi)
+
+
+# Määritellään funktio rasvaprosentin laskentaan
+def aikuisen_rasvaprosentti(bmi, ika, sukupuoli):
+    """Laskee aikuisen rasvaprosentin
+
+    Args:
+        bmi (float): painoindeksi
+        ika (float): henkilön ikä
+        sukupuoli (float): sukupuoli
+
+    Returns:
+        float: aikuisen rasvaprosentti
+    """
+    rasvaprosentti = 1.20 * bmi + 0.23 * ika - 10.8 * sukupuoli -5.4
+    rasvaprosentti = round(rasvaprosentti)
+    return rasvaprosentti    
+
+oma_bmi = laske_bmi(paino, pituus)
+oma_rasvaprosentti = aikuisen_rasvaprosentti(ika, oma_bmi, sukupuoli)
+
+print('Oma painoindeksisi on', oma_bmi, 'ja kehon rasvaprosentti on', oma_rasvaprosentti)
