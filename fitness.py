cfbc994e975255d073ac5058069fb67368641618
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-20 11:09:54 +0200

    Lisätty funktio

--- fitness.py
+++ fitness.py
@@ -5,9 +5,19 @@
 pituus = 163 # Pituus senttimetreinä (cm)
 paino = 57.1 # Paino kilogrammoina (kg)
 
+'''
 pituus_metreina = pituus / 100
 
 # Lasketaan painoindeksi (BMI)
 bmi = paino / pituus_metreina **2
 
-print('Painoindeksisi on', bmi)
+print('Painoindeksisi on ', bmi)
+
+'''
+# Määritellään funktio painoindeksi laskentaan
+def laske_bmi(paino, pituus):
+    pituus = pituus / 100 # Muutetaan pituus metreiksi
+    bmi = paino / pituus**2
+    print('Painoindeksisi on ', bmi)
+
+laske_bmi(57.1, 163)
