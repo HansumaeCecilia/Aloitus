89cd9ceb1cb84a7aceee862186884b53e8abe544
HansumaeCecilia <48033@edu.raseko.fi>
2023-01-27 12:30:37 +0200

    Opelta tarkistus

--- fitness.py
+++ fitness.py
@@ -4,17 +4,6 @@
 # Muuttujat
 # Kysytään käyttäjältä tiedot
 
-pituus_teksti = input('Kuinka pitkä olet? (cm): ') # Input lukee käyttäjän vastaukset
-paino_teksti = input('Kuinka paljon painat? (kg): ')
-# ika_teksti = input('Minkä ikäinen olet? ')
-# sukupuoli_teksti = input('Sukupuoli mies, vastaa 1, N -nainen: vastaa 0: ')
-
-# Muutetaan vastaukset liukuluvuiksi
-pituus = float(pituus_teksti)
-paino = float(paino_teksti)
-# ika = float(ika_teksti)
-# sukupuoli = float(sukupuoli_teksti)
-
 # Määritellään funktio painoindeksi laskentaan
 def laske_bmi(paino, pituus):
 
@@ -33,7 +22,7 @@ def laske_bmi(paino, pituus):
     bmi = round(bmi, 2) # Pyöristetään bmi kahteen desimaaliin, vaihtoehto 1
     return bmi # Palauta bmi-laskukaavan määritelmä allaolevaan koodiin
 
-bmi = laske_bmi(paino, pituus)
+#bmi = laske_bmi(paino, pituus)
 
 
 # Määritellään funktio rasvaprosentin laskentaan
@@ -93,4 +82,5 @@ if ika >= 18:
 else:
     oma_rasvaprosentti = aikuisen_rasvaprosentti(ika, oma_bmi, sukupuoli)
 
-print('Oma painoindeksisi on', oma_bmi, 'ja kehon rasvaprosentti on', oma_rasvaprosentti)
+print('Oma painoindeksisi on', oma_bmi,
+     'ja kehon rasvaprosentti on', oma_rasvaprosentti)
